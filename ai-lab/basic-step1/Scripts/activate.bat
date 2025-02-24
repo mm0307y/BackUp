60204fee5422bf70e9677925da28c867764b8193
DESKTOP-6279PG5\Tae <mm0307a@gmail.com>
2025-02-24 18:00:00 +0900

    2025년 02월 24일 수업 끝

--- ai-lab/basic-step1/Scripts/activate.bat
+++ ai-lab/basic-step1/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Java_Pyhon\ai-lab\basic-step1
+set VIRTUAL_ENV=C:\Users\mtwd9\Desktop\수업백업\ai-lab\basic-step1
 
 if not defined PROMPT set PROMPT=$P$G
 
